4cf4cef6c2c827b6329c0db6a091bb062b46c7b7
Sousa-Edson <edson3711.es@gmail.com>
2022-10-24 18:37:40 -0300

    alter-cor inserido na tabela

--- src/main/java/com/edson/AdoptEasy/controller/PetCorController.java
+++ src/main/java/com/edson/AdoptEasy/controller/PetCorController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,16 +14,16 @@ import com.edson.AdoptEasy.model.PetCor;
 @Controller
 public class PetCorController {
 	private static List<PetCor> listaPet = new ArrayList<PetCor>();
-
-	@GetMapping("/pet/cor")
-	public String cor() {
-		return "pet/cor";
-	}
-	
+//
+//	@GetMapping("/pet/cor")
+//	public String cor() {
+//		return "pet/cor";
+//	}
+//	
 	@PostMapping("/pet/cor")
 	public ModelAndView novo(PetCor cor) {
 
-		ModelAndView mv = new ModelAndView("pet/cor");
+		ModelAndView mv = new ModelAndView("redirect:../pet/cor");
 
 		listaPet.add(cor);
 		 
@@ -31,5 +32,10 @@ public class PetCorController {
 		mv.addObject("cor", cor);
 
 		return mv;
+	}
+	@GetMapping("/pet/cor")
+	public String list(Model model) {
+		model.addAttribute("cor", listaPet);
+		return "pet/cor";
 	}
 }
