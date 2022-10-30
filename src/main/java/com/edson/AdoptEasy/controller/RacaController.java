07593d95658320dd4a98d032f4ae5779a080bf4f
Sousa-Edson <edson3711.es@gmail.com>
2022-10-29 23:06:14 -0300

    incluindo uma lista de raças

--- src/main/java/com/edson/AdoptEasy/controller/RacaController.java
+++ src/main/java/com/edson/AdoptEasy/controller/RacaController.java
@@ -14,59 +14,69 @@ import com.edson.AdoptEasy.model.PetRaca;
 
 @Controller
 public class RacaController {
-	private static List<PetRaca> listaPetRaca = new ArrayList<PetRaca>();
+    private static List<PetRaca> listaPetRaca = new ArrayList<PetRaca>();
 
-	public int idraca(PetRaca raca) {
-		return listaPetRaca.size() + 1;
-	}
+    public void carregar() {
+        if (listaPetRaca.size() < 1) {
+            listaPetRaca.add(new PetRaca(1, "Dachshund", true));
+            listaPetRaca.add(new PetRaca(2, "Labrador retriever", true));
+            listaPetRaca.add(new PetRaca(3, "Buldogue francês", true));
+            listaPetRaca.add(new PetRaca(4, "Pastor-alemão", true));
+        }
+    }
 
-	@PostMapping("/pet/pet-raca")
-	public ModelAndView novo(PetRaca raca) {
+    public int idraca(PetRaca raca) {
+        return listaPetRaca.size() + 1;
+    }
 
-		ModelAndView mv = new ModelAndView("redirect:../pet/pet-raca");
-		if (raca.getId() == 0) {
-			raca.setId(idraca(raca));
+    @PostMapping("/pet/pet-raca")
+    public ModelAndView novo(PetRaca raca) {
 
-			if (raca.getRaca().length() < 2) {
-			} else {
-				listaPetRaca.add(raca);
-			}
-		} else {
-			int i = 0;
-			for (PetRaca PetRaca : listaPetRaca) {
-				if (PetRaca.getId() == raca.getId()) {
-					listaPetRaca.set(i, raca);
-				}
-				i++;
-			}
-			System.out.println("tentando alterar -> " + raca.getId());
-		}
-		raca.setAtivo(true);
+        ModelAndView mv = new ModelAndView("redirect:../pet/pet-raca");
+        if (raca.getId() == 0) {
+            raca.setId(idraca(raca));
 
-		System.out.println(raca.getId());
+            if (raca.getRaca().length() < 2) {
+            } else {
+                listaPetRaca.add(raca);
+            }
+        } else {
+            int i = 0;
+            for (PetRaca PetRaca : listaPetRaca) {
+                if (PetRaca.getId() == raca.getId()) {
+                    listaPetRaca.set(i, raca);
+                }
+                i++;
+            }
+            System.out.println("tentando alterar -> " + raca.getId());
+        }
+        raca.setAtivo(true);
 
-		// insert no banco de dados
-		mv.addObject("raca", raca);
+        System.out.println(raca.getId());
 
-		return mv;
-	}
+        // insert no banco de dados
+        mv.addObject("raca", raca);
 
-	@GetMapping("/pet/pet-raca")
-	public String list(Model model) {
-		model.addAttribute("c", new PetRaca());
-		model.addAttribute("raca", listaPetRaca);
-		return "pet/pet-raca";
-	}
+        return mv;
+    }
 
-	@GetMapping("/pet/pet-raca/{id}")
-	public String detalhe(@PathVariable("id") int id, Model model) {
-		for (PetRaca c : listaPetRaca) {
-			if (c.getId() == id) {
-				model.addAttribute("c", c);
-				model.addAttribute("raca", listaPetRaca);
-				break;
-			}
-		}
-		return "pet/pet-raca";
-	}
+    @GetMapping("/pet/pet-raca")
+    public String list(Model model) {
+        carregar();
+        model.addAttribute("c", new PetRaca());
+        model.addAttribute("raca", listaPetRaca);
+        return "pet/pet-raca";
+    }
+
+    @GetMapping("/pet/pet-raca/{id}")
+    public String detalhe(@PathVariable("id") int id, Model model) {
+        for (PetRaca c : listaPetRaca) {
+            if (c.getId() == id) {
+                model.addAttribute("c", c);
+                model.addAttribute("raca", listaPetRaca);
+                break;
+            }
+        }
+        return "pet/pet-raca";
+    }
 }
