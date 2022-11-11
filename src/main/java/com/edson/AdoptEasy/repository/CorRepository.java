cda11212dcb1c0ce28354f423e7ad408d9451c4a
Sousa-Edson <edson3711.es@gmail.com>
2022-11-11 20:34:34 -0300

    indentado

--- src/main/java/com/edson/AdoptEasy/repository/CorRepository.java
+++ src/main/java/com/edson/AdoptEasy/repository/CorRepository.java
@@ -1,11 +1,12 @@
 package com.edson.AdoptEasy.repository;
+
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import com.edson.AdoptEasy.model.PetCor;
 
 public interface CorRepository extends CrudRepository<PetCor, Integer> {
-    
-List<PetCor> findByCor(String cor);
 
-PetCor findById(int id);
-    }
+    List<PetCor> findByCor(String cor);
+
+    PetCor findById(int id);
+}
