ec6037b8ca28de0dc3bde0ebfc0b5587a0cd9d64
Sousa-Edson <edson3711.es@gmail.com>
2022-11-11 22:13:16 -0300

    <11/11 retirando comentarios de petController>

--- src/main/java/com/edson/AdoptEasy/controller/CorController.java
+++ src/main/java/com/edson/AdoptEasy/controller/CorController.java
@@ -1,8 +1,5 @@
 package com.edson.AdoptEasy.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,19 +15,11 @@ import com.edson.AdoptEasy.repository.CorRepository;
 public class CorController {
 	@Autowired
 	private CorRepository repository;
-    
-  
-
-	
 
 	@PostMapping("/pet/pet-cor")
 	public ModelAndView novo(PetCor cor) {
-
 		ModelAndView mv = new ModelAndView("redirect:../pet/pet-cor");
-		
-			repository.save(cor);
-
-
+		repository.save(cor);
 		return mv;
 	}
 
@@ -43,22 +32,22 @@ public class CorController {
 
 	@GetMapping("/pet/pet-cor/{id}")
 	public String detalhe(@PathVariable("id") int id, Model model) {
-		PetCor c = repository.findById(id);;  
-			if (c == null) {
-				return "cor-nao-encontrada";
-				
-			}model.addAttribute("c", c);
-			
+		PetCor c = repository.findById(id);
+		if (c == null) {
+			return "cor-nao-encontrada";
+		}
+		model.addAttribute("c", c);
 		return "pet/pet-cor";
 	}
+
 	@GetMapping("/pet/pet-cor/{id}/edit")
-    public String edit(@PathVariable("id") int id, Model model) {
-        PetCor c = repository.findById(id);;  
-            if (c == null) {
-                return "cor-nao-encontrada";
-                
-            }model.addAttribute("c", c);
-             
-        return "pet/pet-cor";
-	
-}}
+	public String edit(@PathVariable("id") int id, Model model) {
+		PetCor c = repository.findById(id);
+		if (c == null) {
+			return "cor-nao-encontrada";
+		}
+		model.addAttribute("c", c);
+		return "pet/pet-cor";
+
+	}
+}
--- src/main/java/com/edson/AdoptEasy/controller/PetController.java
+++ src/main/java/com/edson/AdoptEasy/controller/PetController.java
@@ -7,75 +7,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PetController {
 
-//	private static List<Pet> listaPet = new ArrayList<Pet>();
-
-	 
-
-//	@GetMapping("/pet")
-//	public String form() {
-//		return "pet/pet-form";
-//	}
-
-//	@PostMapping("/pet")
-//	public String novo(
-//			@RequestParam("id") int id,
-//			@RequestParam("nome") String nome,
-//			@RequestParam("raca") String raca,
-//			@RequestParam("idade") int idade,
-//			Model model) {
-//		
-//		model.addAttribute("id", id);
-//		model.addAttribute("nome", nome);
-//		model.addAttribute("raca", raca);
-//		model.addAttribute("idade", idade);
-//		
-//		return "pet/pet-criado";
-//	}
-
 	@GetMapping("/pet")
 	public String novo() {
 		return "pet/home";
 	}
+
 	@PostMapping("/pet")
 	public String home() {
 		return "pet/home";
 	}
+
 	@GetMapping("/pet/login")
 	public String login() {
 		return "pet/login";
 	}
-
-	
-  
-	
-//	@PostMapping("/pet")
-//	public ModelAndView novo(Pet pet) {
-//
-//		ModelAndView mv = new ModelAndView("pet/pet-criado");
-//
-//		listaPet.add(pet);
-//
-//		// insert no banco de dados
-//		mv.addObject("pet", pet);
-//
-//		return mv;
-//	}
-
-//	@GetMapping("/pet/list")
-//	public String list(Model model) {
-//		model.addAttribute("pet", listaPet);
-//		return "pet/pet-list";
-//	}
-//
-//	@GetMapping("/pet/{id}")
-//	public String detalhe(@PathVariable("id") int id, Model model) {
-//		for (Pet p : listaPet) {
-//			if (p.getId() == id) {
-//				model.addAttribute("pet", p);
-//				break;
-//			}
-//		}
-//		return "pet/pet-detalhe";
-//	}
 
 }
